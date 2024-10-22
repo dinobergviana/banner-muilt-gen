6e742c23d77878008303dc91530ce53ce915b686
Dinobergue Viana <dinobergueviana@gmail.com>
2024-10-22 17:19:43 +0000

    refatora metodo readAndSaveCSVFile

--- src/app.js
+++ src/app.js
@@ -4,40 +4,61 @@ import csv from "csv-parser";
 import { generateBannerImageFeed } from "./services/generateBannerFeed.js";
 import { generateBannerImageStories } from "./services/generateBannerStories.js";
 
-const PATH_BANNERS_TERESINA_FEED =
-  "./src/banners/generatedBanners/teresina/feed";
-const PATH_BANNERS_PARNAIBA_FEED =
-  "./src/banners/generatedBanners/parnaiba/feed";
-
-const PATH_BANNERS_TERESINA_STORIES =
-  "./src/banners/generatedBanners/teresina/stories";
-const PATH_BANNERS_PARNAIBA_STORIES =
-  "./src/banners/generatedBanners/parnaiba/stories";
+const PATH_BANNERS = "./src/banners/generatedBanners";
 
 let gcInfo = [];
 
 const readAndSaveCSVFile = async () => {
-  // FEED
-  fs.createReadStream("src/banners-csv-teresina.csv")
+  fs.createReadStream("src/banners.csv")
     .pipe(csv())
     .on("data", (data) => {
       gcInfo.push(data);
     })
     .on("end", () => {
-      gcInfo.forEach((gc) =>
-        generateBannerImageFeed(gc, PATH_BANNERS_TERESINA_FEED)
-      );
-    });
-  // STORIES
-  fs.createReadStream("src/banners-csv-teresina.csv")
-    .pipe(csv())
-    .on("data", (data) => {
-      gcInfo.push(data);
-    })
-    .on("end", () => {
-      gcInfo.forEach((gc) =>
-        generateBannerImageStories(gc, PATH_BANNERS_TERESINA_STORIES)
-      );
+      const totalBanners = gcInfo.length;
+      let processedCount = 0;
+
+      const updateProgress = () => {
+        processedCount++;
+        const percentage = (
+          (processedCount / (totalBanners * 2)) *
+          100
+        ).toFixed(2);
+        process.stdout.write(
+          `\rProgresso: ${percentage}% (${processedCount}/${
+            totalBanners * 2
+          }) banners processados`,
+        );
+      };
+
+      const handleError = (error, bannerType, bannerData) => {
+        console.error(
+          `\nErro ao gerar banner ${bannerType} para o item: ${JSON.stringify(
+            bannerData,
+          )}`,
+        );
+        console.error(`Detalhes do erro: ${error.message}`);
+      };
+
+      gcInfo.forEach((gc) => {
+        try {
+          generateBannerImageFeed(gc, PATH_BANNERS);
+          updateProgress();
+        } catch (error) {
+          handleError(error, "Feed", gc);
+        }
+      });
+
+      gcInfo.forEach((gc) => {
+        try {
+          generateBannerImageStories(gc, PATH_BANNERS);
+          updateProgress();
+        } catch (error) {
+          handleError(error, "Stories", gc);
+        }
+      });
+
+      process.stdout.write("\nAguardando a liberação do terminal...\n");
     });
 };
 
